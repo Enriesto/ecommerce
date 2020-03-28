29a7d8c2d098740ba6396817b23962d10cc4262c
Ernest Janzen <ejjthehun@hotmail.com>
2020-03-28 12:55:12 -0500

    Created User, Address models.

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,6 @@
 class Order < ApplicationRecord
   validates :order_number, presence: true
 
-  belongs_to :statuses
+  belongs_to :statuses, :users
   has_many :order_products
 end
---
+++ db/migrate/20200328173259_create_addresses.rb
@@ -0,0 +1,12 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string :street_number
+      t.string :city
+      t.string :province
+      t.int :user_id
+
+      t.timestamps
+    end
+  end
+end
