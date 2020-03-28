ef3a1eb376e838ed915abdccdcefd5049b5607b6
Ernest Janzen <ejjthehun@hotmail.com>
2020-03-28 13:02:44 -0500

    Added more validation to Product and User models

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :product_number, presence: true, uniqueness: true
   validates :quantity_in_stock, numericality: { only_integer: true }
+  validates :description, length: { maximum: 500 }
 
   belongs_to :categories
   has_many :order_products
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :email, confirmation: true
+  validates :password, length: { minimum: 8 }
+
   has_many :orders, :addresses
 end
