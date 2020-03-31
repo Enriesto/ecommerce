b5ef79705fe91b8566da3f88e471830248ac9dff
Ernest Janzen <ejjthehun@hotmail.com>
2020-03-31 16:26:47 -0500

    Added content to products view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
+  root to: 'products#index'
+
+  get 'products/:id', to: 'products#show', id: /\d+/
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
