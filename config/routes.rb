2ffff3ffbb4015f3cae99a568a8ed74b2bc9afe5
Ernest Janzen <ejjthehun@hotmail.com>
2020-04-04 13:20:02 -0500

    Added a product route and header styling.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'categories/show'
   root to: 'products#index'
 
-  get 'products/:id', to: 'products#show', id: /\d+/
+  get 'products/:id', to: 'products#show', id: /\d+/, as: 'product' # product_path
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
